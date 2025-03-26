1703db180fc926c0570384197e955c1c7c822b51
SOGLIUZZO Loris <sogliuzzo.loris@gmail.com>
2025-03-26 10:26:34 +0100

    La commande start affiche chaque seconde dans la console le temps passé depuis son execution

--- minecraft-uhc/src/main/java/com/sogliuzzo/Main.java
+++ minecraft-uhc/src/main/java/com/sogliuzzo/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.sogliuzzo.commands.StartCommand;
 
 public class Main extends JavaPlugin {
+    public int time = 0;
     @Override
     public void onEnable() {
         getCommand("start").setExecutor(new StartCommand(this));
--- minecraft-uhc/src/main/java/com/sogliuzzo/commands/StartCommand.java
+++ minecraft-uhc/src/main/java/com/sogliuzzo/commands/StartCommand.java
@@ -1,9 +1,13 @@
 package com.sogliuzzo.commands;
 
+import java.util.logging.Level;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import com.sogliuzzo.Main;
 
@@ -17,8 +21,19 @@ public class StartCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Bukkit.getLogger().info("Starting the game.");
-        // Bukkit.getScheduler().runTask(main, null);
+        Bukkit.getLogger().log(Level.INFO ,"Starting the game.");
+        main.time = 0;
+        BukkitRunnable bukkitRunnable = new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                main.time++;
+                Bukkit.getLogger().log(Level.INFO, "" + main.time);
+            }
+
+        };
+
+        bukkitRunnable.runTaskTimer(main, 0, 20);
         return true;
     }
 
